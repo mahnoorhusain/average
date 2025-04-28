0212f6a10e11b3a99a1f22dcaceb8933be3370ff
mahnoorhusain <mahnooorhusain@gmail.com>
2025-04-28 22:52:42 +0500

    Add files via upload

---
+++ average.js
@@ -0,0 +1,12 @@
+let age1 =parseInt (prompt (`enter the age of the 1st student: `));
+let age2 =parseInt (prompt (`enter the age of the 2nd student: `));
+let age3 =parseInt (prompt (`enter the age of the 3rd student: `));
+let age4 =parseInt (prompt (`enter the age of the 4th student: `));
+let age5 =parseInt (prompt (`enter the age of the 5th student: `));
+let age6 =parseInt (prompt (`enter the age of the 6th student: `));
+let age7 =parseInt (prompt (`enter the age of the 7th student: `));
+let age8 =parseInt (prompt (`enter the age of the 8th student: `));
+let age9 =parseInt (prompt (`enter the age of the 9th student: `));
+let age10 =parseInt (prompt (`enter the age of the 10th student: `));
+let average = (age1+age2+age3+age4+age5+age6+age7+age8+age9+age10) / 10
+console.log (`the average age of the class is: ${average}`) 
